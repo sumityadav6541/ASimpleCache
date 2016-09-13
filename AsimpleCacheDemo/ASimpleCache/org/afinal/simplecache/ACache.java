db5022776eb39e0ae60d748b490428828056336c
Sumit Yadav <sumityadav6541@gmail.com>
2016-09-13 16:03:26 +0530

    added thread safety for the manager Acache initialization.
    added thread safety for the manager Acache initialization.

--- AsimpleCacheDemo/ASimpleCache/org/afinal/simplecache/ACache.java
+++ AsimpleCacheDemo/ASimpleCache/org/afinal/simplecache/ACache.java
@@ -83,8 +83,12 @@ public class ACache {
 	public static ACache get(File cacheDir, long max_zise, int max_count) {
 		ACache manager = mInstanceMap.get(cacheDir.getAbsoluteFile() + myPid());
 		if (manager == null) {
-			manager = new ACache(cacheDir, max_zise, max_count);
-			mInstanceMap.put(cacheDir.getAbsolutePath() + myPid(), manager);
+			synchronized(this){
+				if(manager == null){
+					manager = new ACache(cacheDir, max_zise, max_count);
+					mInstanceMap.put(cacheDir.getAbsolutePath() + myPid(), manager);
+				}
+			}
 		}
 		return manager;
 	}
